b2deef6b915c0c1b57d43bfafbae9c2efe3082be
Harvie-Mz <69393918+Harvie-Mz@users.noreply.github.com>
2021-03-25 09:14:58 +0200

    Update and rename README.md to A.-Ehab-and-another-construction-problem.cpp

---
+++ A.-Ehab-and-another-construction-problem.cpp
@@ -0,0 +1,15 @@
+# A.-Ehab-and-another-construction-problem
+
+
+#include <iostream>
+using namespace std;
+int main (){
+	int a,b,c;
+	cin >> a;
+if (a==1)
+cout <<-1;
+else 
+cout << a << " "<<a;
+return 0;
+}
+  
